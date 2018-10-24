a6c11daa47aef2bb90fe740453ca6b232abc41b1
kjefferson1992 <40824814+kjefferson1992@users.noreply.github.com>
2018-10-24 07:11:38 +0200

    Add files via upload

---
+++ gameofchats3/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  gameofchats
+//
+//  Created by Brian Voong on 6/24/16.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
+    }
+    
+    @objc func handleLogout() {
+        
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+
+}
+
